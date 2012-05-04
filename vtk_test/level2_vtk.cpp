adcf4ab51ab36bedff7846074ef585ffe18346f1
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2012-05-04 12:35:11 -0400

    No need to call clear and clear color as render should take care of this

--- vtk_test/level2_vtk.cpp
+++ vtk_test/level2_vtk.cpp
@@ -180,8 +180,8 @@ int main(int argc, char * argv[]){
   for(int i = 0; i < 1000; ++i)
   {
     ren1->GetActiveCamera()->Azimuth(1);
-    glClearColor (0.0,0.0,0.0,1.0);
-    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    //glClearColor (0.0,0.0,0.0,1.0);
+    //glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     renWin->Render();
   }
 
